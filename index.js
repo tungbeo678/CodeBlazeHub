5128d95294c4f00f2c06b6b79f0b96bdde3662dd
tungbeo678 <nelsonnapapun0suy@hotmail.com>
2024-05-14 13:51:17 +0700

    demo

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-function findNumbers(nums) {
-  return nums.filter((num) => num.toString().length % 2 === 0).length;
-}
+const cocktailShakerSort = (arr) => {
+  let swapped = true;
+  let start = 0;
+  let end = arr.length - 1;
+  while (swapped) {
+    swapped = false;
+    for (let i = start; i < end; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+    swapped = false;
+    end--;
+    for (let i = end - 1; i >= start; i--) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+    start++;
+  }
+  return arr;
+};
